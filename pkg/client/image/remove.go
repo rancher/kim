1eef29e773b424c31ec859089d598af82a059922
Jacob Blain Christen <jacob@rancher.com>
2021-02-09 23:02:21 -0700

    image/remove: emulate docker's untagging
    When the Docker CLI removes an image:tag it effectively removes the tag.
    This change emulates such behavior but will fallback to the CRI
    implementation if an "image not found" error is returned from the
    containerd-native delete. This is typically only the case when passing
    image identifiers (truncated or otherwise) incurs behavior equivalent to
    `docker image rm --force --no-prune <image-id>`.
    
    Signed-off-by: Jacob Blain Christen <jacob@rancher.com>

--- pkg/client/image/remove.go
+++ pkg/client/image/remove.go
@@ -14,14 +14,23 @@ type Remove struct {
 }
 
 func (s *Remove) Do(ctx context.Context, k8s *client.Interface, image string) error {
-	if named, err := reference.ParseNormalizedNamed(image); err == nil {
-		image = named.String()
-	}
 	return client.Images(ctx, k8s, func(ctx context.Context, imagesClient imagesv1.ImagesClient) error {
 		req := &imagesv1.ImageRemoveRequest{
 			Image: &criv1.ImageSpec{
 				Image: image,
 			},
+		}
+		if ref, err := reference.ParseAnyReference(image); err == nil {
+			statusRequest := imagesv1.ImageStatusRequest{
+				Image: &criv1.ImageSpec{
+					Image: ref.String(),
+				},
+			}
+			statusResponse, err := imagesClient.Status(ctx, &statusRequest)
+			logrus.Debugf("%#v", statusResponse.Image)
+			if err == nil && statusResponse.Image != nil {
+				req.Image = statusRequest.Image
+			}
 		}
 		res, err := imagesClient.Remove(ctx, req)
 		if err != nil {
